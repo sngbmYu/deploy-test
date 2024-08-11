53a9f285b700254a3cdb1ffb762df7cd99d37c97
yu seungbeom <dldkdlelqnzo@gmail.com>
2024-08-12 03:38:25 +0900

    github action v6

---
+++ src/main/java/com/hrth/ustock/controller/HealthController.java
@@ -0,0 +1,18 @@
+package com.hrth.ustock.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class HealthController {
+
+    @GetMapping("/health")
+    public ResponseEntity<Void> checkHealth() {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+}
