f5c33757e5158702b226860c55359b45185fee3d
yu seungbeom <dldkdlelqnzo@gmail.com>
2024-08-12 01:43:29 +0900

    github action v1

---
+++ src/main/java/com/hrth/ustock/UstockApplication.java
@@ -0,0 +1,13 @@
+package com.hrth.ustock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UstockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UstockApplication.class, args);
+	}
+
+}
